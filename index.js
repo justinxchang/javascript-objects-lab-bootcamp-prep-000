aaedb2b516d3cdc9cdd802e55a78785a2383567e
Justin Chang <justinxchang@hotmail.com>
2018-08-17 21:12:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function deleteFromObjectByKey(object, key){
+
+
+function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key];
   return object;
 }
